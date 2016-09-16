0e992368aeac349767ade3572e55aec3d84ca756
Finn <finn@finn.io>
2016-09-15 22:12:58 -0700

    Example uses hci1 cuz thats what I use

--- example.sh
+++ example.sh
@@ -5,7 +5,7 @@ if [ "$?" != "0" ]; then
   exit $?
 fi
 
-hciconfig hci0 up
-hcitool cmd 0x08 0x000a 00
-hcitool cmd 0x08 0x0008 $msg
-hcitool cmd 0x08 0x000a 01
+hciconfig hci1 up
+hcitool -i hci1 cmd 0x08 0x000a 00
+hcitool -i hci1 cmd 0x08 0x0008 $msg
+hcitool -i hci1 cmd 0x08 0x000a 01
